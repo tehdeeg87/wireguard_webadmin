7815216fd710f0212a70812d1a5a2209dd4d7017
tehdeeg87 <dgillis77@gmail.com>
2025-09-30 11:04:17 -0600

    persistent database

--- wireguard_webadmin/settings.py
+++ wireguard_webadmin/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'wireguard_webadmin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
 
